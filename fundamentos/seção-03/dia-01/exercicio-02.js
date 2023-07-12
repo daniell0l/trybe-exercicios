2aaf43e3126c436d50fc9eebfa7af5b01ab84b11
daniell0l <daniel_l0l@hotmail.com>
2023-07-12 20:08:13 -0300

    feat: exercicio 2 do dia 1

---
+++ fundamentos/seção-03/dia-01/exercicio-02.js
@@ -0,0 +1,11 @@
+const num1 = 50;
+const num2 = 200
+let message = ''
+
+if ( num1 > num2) {
+  message = 'O numero 1 é o maior numero!'
+} else {
+  message = 'O numero 2 é o maior'
+}
+
+console.log(message);
