b63f062aff80f89ff839ea0e09fbb530b4704695
daniell0l <daniel_l0l@hotmail.com>
2023-09-05 21:55:18 -0300

    FEAT: finalizando curso de fundamentos

---
+++ fundamentos/seção-04/dia-02/buscando-elementos/script.js
@@ -0,0 +1,20 @@
+const whereAreYou = document.getElementById('where-are-you');
+
+const parent = whereAreYou.parentNode;
+parent.style.backgroundColor = 'blue';
+
+const firstChildOfChild = document.getElementById('first-child-of-child');
+firstChildOfChild.innerText = 'vamos que vamos';
+
+const firstChild = parent.firstElementChild;
+
+const firstChildTow = document.getElementById('where-are-you');
+firstChildTwo.previousElementSibling;
+
+const attention = document.getElementById('where-are-you');
+attention.nextSibling
+
+const thirdChild = document.getElementById('where-are-you');
+thirdChild.nextElementSibling;
+
+const thirdChildTwo = parent.lastElementChild.previousElementSibling;
